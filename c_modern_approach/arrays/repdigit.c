a46ef1272f1fa423a48f6352b78e0c6705bae68b
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2024-09-05 20:41:36 -0300

    Update repdigit.c

--- c_modern_approach/arrays/repdigit.c
+++ c_modern_approach/arrays/repdigit.c
@@ -5,29 +5,33 @@
 
 int main(int argc, char const *argv[])
 {
-    bool digit_seen[10] = {false};
+    int digit_count[10] = {0};
     int digit;
     long n;
 
     printf("Enter a number: ");
     scanf("%ld", &n);
 
+    long original_n = n;
+
     while (n > 0)
     {
         digit = n % 10;
-        if (digit_seen[digit]) {
-            break;
-        }
-        digit_seen[digit] = true;
+        digit_count[digit]++;
         n /= 10;
     }
 
-    if (n > 0) {
-        printf("%ld  Repeated digit\n", n);
-    } else {
-        printf("No repeated digit\n");
+    printf("Digit:      ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", i);
     }
+    printf("\nOccurrences:");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", digit_count[i]);
+    }
+    printf("\n");
     
+    printf("\n");
 
     return 0;
 }
